73b5f63202fac362d8fa165277e6c770603665bc
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2024-12-04 13:27:54 +0100

    refactor day 1 part 1

--- day01/main.go
+++ day01/main.go
@@ -10,37 +10,44 @@ import (
 	"strings"
 )
 
+func abs(n int) int {
+	if n > 0 {
+		return n
+	}
+	return -n
+}
+
 func main() {
-	var l1, l2 []int
+	var list1, list2 []int
 
 	scan := bufio.NewScanner(os.Stdin)
 	for scan.Scan() {
 		line := scan.Text()
 		fields := strings.Fields(line)
 
-		n, _ := strconv.Atoi(fields[0])
-		l1 = append(l1, n)
+		n1, err := strconv.Atoi(fields[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		list1 = append(list1, n1)
 
-		m, _ := strconv.Atoi(fields[1])
-		l2 = append(l2, m)
+		n2, err := strconv.Atoi(fields[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		list2 = append(list2, n2)
 	}
 	if err := scan.Err(); err != nil {
 		log.Fatal(err)
 	}
-	sort.Ints(l1)
-	sort.Ints(l2)
+
+	sort.Ints(list1)
+	sort.Ints(list2)
 
 	sum := 0
-	for i := 0; i < len(l1); i++ {
-		d := l1[i] - l2[i]
+	for i := 0; i < len(list1); i++ {
+		d := list1[i] - list2[i]
 		sum += abs(d)
 	}
 	fmt.Println(sum)
-}
-
-func abs(n int) int {
-	if n > 0 {
-		return n
-	}
-	return -n
 }
