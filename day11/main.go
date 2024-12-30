71df7b71cf4a44db5e6e645617b4b9033a6b112b
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2024-12-31 00:07:49 +0100

    day 11 part 2

--- day11/main.go
+++ day11/main.go
@@ -46,6 +46,44 @@ func blink(cur []int) []int {
 	return next
 }
 
+type input struct {
+	stone, blinks int
+}
+
+func count(stone, blinks int, memory map[input]int) int {
+	if blinks == 0 {
+		return 1
+	}
+
+	if result, ok := memory[input{stone, blinks}]; ok {
+		return result
+	}
+
+	if stone == 0 {
+		result := count(1, blinks-1, memory)
+		memory[input{1, blinks - 1}] = result
+		return result
+	}
+	if s := strconv.Itoa(stone); len(s)%2 == 0 {
+		mid := len(s) / 2
+		s1, s2 := s[:mid], s[mid:]
+		n1, _ := strconv.Atoi(s1)
+		n2, _ := strconv.Atoi(s2)
+
+		res1 := count(n1, blinks-1, memory)
+		memory[input{n1, blinks - 1}] = res1
+
+		res2 := count(n2, blinks-1, memory)
+		memory[input{n2, blinks - 1}] = res2
+
+		return res1 + res2
+	}
+
+	res := count(stone*2024, blinks-1, memory)
+	memory[input{stone * 2024, blinks - 1}] = res
+	return res
+}
+
 func main() {
 	n := flag.Int("n", 25, "number of blinks")
 	flag.Parse()
@@ -54,11 +92,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// fmt.Println(cur)
-	for i := 0; i < *n; i++ {
-		cur = blink(cur)
-		// fmt.Println(cur)
+	sum := 0
+	for _, stone := range cur {
+		sum += count(stone, *n, make(map[input]int))
 	}
-	fmt.Println(len(cur))
+	fmt.Println(sum)
 }
